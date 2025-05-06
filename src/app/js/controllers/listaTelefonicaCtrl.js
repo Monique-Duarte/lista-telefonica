cd74af6ed53c1fe76beac232c5c3e9bd386dbed7
Monique Duarte <monique.duarte@selsyn.com.br>
2025-05-06 11:04:31 -0300

    Remove unused files and update project structure

--- src/app/js/controllers/listaTelefonicaCtrl.js
+++ src/app/js/controllers/listaTelefonicaCtrl.js
@@ -1,29 +1,21 @@
-angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($scope, $http) {
+angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($scope, uppercaseFilter) {
   $scope.app = "Lista Telefonica";
-  $scope.contatos = [];
-  $scope.operadoras = [];
-
-  var carregarContatos = function () {
-    $http.get("http://localhost:3412/contatos").success(function (data) {
-      $scope.contatos = data;
-    }).error(function (data, status) {
-      $scope.message = "Aconteceu um problema: " + data;
-    });
-  };
-
-  var carregarOperadoras = function () {
-    $http.get("http://localhost:3412/operadoras").success(function (data) {
-      $scope.operadoras = data;
-    });
-  };
-
+  $scope.contatos = [
+    { nome: uppercaseFilter("Pedro"), telefone: "9999-8888", data: new Date(), operadora: { nome: "Oi", codigo: 14, categoria: "Celular" } },
+    { nome: "Ana", telefone: "9999-8877", data: new Date(), operadora: { nome: "Vivo", codigo: 15, categoria: "Celular" } },
+    { nome: "Maria", telefone: "9999-8866", data: new Date(), operadora: { nome: "Tim", codigo: 41, categoria: "Celular" } }
+  ];
+  $scope.operadoras = [
+    { nome: "Oi", codigo: 14, categoria: "Celular", preco: 2 },
+    { nome: "Vivo", codigo: 15, categoria: "Celular", preco: 1 },
+    { nome: "Tim", codigo: 41, categoria: "Celular", preco: 3 },
+    { nome: "GVT", codigo: 25, categoria: "Fixo", preco: 1 },
+    { nome: "Embratel", codigo: 21, categoria: "Fixo", preco: 2 }
+  ];
   $scope.adicionarContato = function (contato) {
-    contato.data = new Date();
-    $http.post("http://localhost:3412/contatos", contato).success(function (data) {
-      delete $scope.contato;
-      $scope.contatoForm.$setPristine();
-      carregarContatos();
-    });
+    $scope.contatos.push(angular.copy(contato));
+    delete $scope.contato;
+    $scope.contatoForm.$setPristine();
   };
   $scope.apagarContatos = function (contatos) {
     $scope.contatos = contatos.filter(function (contato) {
@@ -39,7 +31,4 @@ angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($s
     $scope.criterioDeOrdenacao = campo;
     $scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao;
   };
-
-  carregarContatos();
-  carregarOperadoras();
 });
